ffb7e81e7b007356b9aaf7621c72cc1b5284cfca
ShaunFChen <shaunfchen@gmail.com>
2020-11-04 17:33:17 -0800

    update R script.

--- required_tools/tgp_pca.r
+++ required_tools/tgp_pca.r
@@ -7,10 +7,12 @@ df <- read.csv(eigenvec_fp, sep = ' ', header = 0)
 pop <- read.csv(pop_fp, sep = '\t')
 df_ <- merge(df, pop, by.x = 'V1', by.y = 'IID')
 
-png("figure_tgp_pca.png", width = 6, height = 6, units = "in", res = 300)
+png("figure_tgp_pca.png", width = 12, height = 6, units = "in", res = 300)
 
 print({
     ggplot(df_, aes(x = V3, y = V4, color = sup_pop)) + geom_point(size = 1, alpha = 0.5) + xlab("PC1") + ylab("PC2")
 })
                     
 dev.off()
+
+print("output generated: figure_tgp_pca.png")
